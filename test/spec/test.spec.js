8d15b8adb5703949ac457dfc8a912e15cfc808fe
Brett Uglow <buglow@odecee.com.au>
2016-01-13 08:20:06 +1100

    chore(CI): verify new Travis/Mocha approach to get output

--- test/spec/test.spec.js
+++ test/spec/test.spec.js
@@ -2,20 +2,36 @@
 
 var path = require('path');
 var assert = require('assert');
+var server = require('./../server');
 
 var fixtureDir = process.env.FIXTURE_DIR;
 var fixtureFileName = process.env.FIXTURE;   // Passed from the testRunner
 var tempTestDir = process.env.TEST_DIR;
 
-describe('x', function() {
-  it('simple test', function() {
 
-    // The actual tests
-    assert.equal(1 + 1, 4);
+//module.exports = function() {
 
+  describe('npm run dev TEST', function() {
+
+    var baseUrl;
+
+    before(function() {
+      // Start up the confit DEV webserver
+      return server.start(tempTestDir, 'serverDev', 10000).then(function success(result) {
+        baseUrl = result.baseUrl;
+      });
+    });
+
+    it('should pass, mate', function() {
+      assert.equal(1 + 1, 2);
+    });
+
+    it('should fail, man', function() {
+      assert.equal(2 + 2, 10);
+    });
+
+    after(function() {
+      server.stop();
+    });
   });
-
-  // The mocha --delay flag provides a root-level run() callback, signifying that we have finished the initial asynchronous processing
-  //run();
-});
-
+//};
