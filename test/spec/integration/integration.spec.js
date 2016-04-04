0c5728e7385762c2a28cd24bfdcfc7e408e6c63f
Brett Uglow <buglow@odecee.com.au>
2016-04-04 20:55:34 +1000

    chore(ci): print file list (printing package.json failed??!?!?)

--- test/spec/integration/integration.spec.js
+++ test/spec/integration/integration.spec.js
@@ -3,10 +3,11 @@
 var fixtureFileName = process.env.FIXTURE;   // Passed from the testRunner
 var testDir = process.env.TEST_DIR;
 var confitConfig = require(testDir + 'confit.json')['generator-confit'];
-
+var fs = require('fs');
 
 describe('test "' + fixtureFileName + '"', function () {
-  console.info(require(testDir + 'package.json'));
+  //console.info(require(testDir + 'package.json'));
+  fs.readdirSync(testDir).forEach(file => console.log(file));
   // The actual tests...
   require('./testDev')(confitConfig);
   require('./testBuildServe')(confitConfig);
