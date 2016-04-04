1e87a9cfb7abd890139627becb2349bf058c5512
Brett Uglow <buglow@odecee.com.au>
2016-04-04 22:23:58 +1000

    chore(ci): still trying to determine root cause - the unit tests are not running properly

--- test/spec/unit/unitTestUtils.js
+++ test/spec/unit/unitTestUtils.js
@@ -13,22 +13,29 @@ function runGenerator(generatorName, confitFixture, beforeTestCb, assertionCb) {
   var generatorName = path.join(__dirname, '../../../generators/' + generatorName);
   var testDir;
 
-  return helpers.run(generatorName)
-    .inTmpDir(function (dir) {
-      // Copy the confit.json fixture here
-      if (confitFixture) {
-        fs.copySync(path.join(__dirname, 'fixtures/', confitFixture), path.join(dir, 'confit.json'));
-      }
-      testDir = dir;
-      beforeTestCb(testDir);
-    })
-    .withArguments(['--force=true'])    // Any file-conflicts, over-write
-    //.withGenerators(generators)
-    .withOptions({
-      skipInstall: true,
-      skipRun: true
-    })
-    .on('end', function() {
-      assertionCb(testDir);
-    });
+  try {
+    console.log(arguments);
+    return helpers.run(generatorName)
+      .inTmpDir(function(dir) {
+        // Copy the confit.json fixture here
+        if (confitFixture) {
+          console.log(path.join(__dirname, 'fixtures/', confitFixture));
+          fs.copySync(path.join(__dirname, 'fixtures/', confitFixture), path.join(dir, 'confit.json'));
+        }
+        console.log('testdir', dir);
+        testDir = dir;
+        beforeTestCb(testDir);
+      })
+      .withArguments(['--force=true'])    // Any file-conflicts, over-write
+      //.withGenerators(generators)
+      .withOptions({
+        skipInstall: true,
+        skipRun: true
+      })
+      .on('end', function() {
+        assertionCb(testDir);
+      });
+  } catch (e) {
+    console.error(e);
+  }
 }
