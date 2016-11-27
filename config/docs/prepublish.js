dd01b64819b2584ba11fb20f7a9dec47b5f02ee0
Brett Uglow <buglow@odecee.com.au>
2016-11-27 12:15:30 +1100

    chore(build): upgrade config by running `yo confit`
    fix/add-angular2-router-to-vendor-scripts

--- config/docs/prepublish.js
+++ config/docs/prepublish.js
@@ -1,11 +1,30 @@
 'use strict';
 
 // START_CONFIT_GENERATED_CONTENT
-// Update the swanky.config.yaml file's version number
+const path = require('path');
+const basePath = path.join(__dirname, '/../../');
 const latestVersion = require('latest-version');
 const fs = require('fs');
 const pkgName = 'generator-confit';
 const pkgRepo = 'generator-confit';
+
+const IS_DRYRUN = process.argv.indexOf('-d') > -1;    // Allow a dry-run to see what changes would be made
+
+let FILES_TO_PROCESS = [
+  // Update the swanky.config.yaml file's version number and server path
+  {
+    name: 'docs/swanky.config.yaml',
+    searchRE: /^version:.*$/m,
+    replacement: (version) => `version: ${version}`
+  },
+  {
+    name: 'docs/swanky.config.yaml',
+    searchRE: /^serverPath:.*$/m,
+    replacement: () => `serverPath: ${pkgRepo}`
+  }
+];
+
+// In the next section you can modify FILES_TO_PROCESS to meet your needs...
 // END_CONFIT_GENERATED_CONTENT
 
 
@@ -13,13 +32,22 @@ const pkgRepo = 'generator-confit';
 latestVersion(pkgName).then(version => {
   console.log(`Latest version of ${pkgName}: ${version}`);
 
-  // Read Swanky config YAML file, modify the version and server-path, then save it again
-  let text = fs.readFileSync('docs/swanky.config.yaml', 'utf8');
+  // Patch the files
+  FILES_TO_PROCESS.forEach(file => {
+    let fileName = path.join(basePath, file.name);
+    let text = fs.readFileSync(fileName, 'utf8');
 
-  text = text.replace(/^version:.*$/m, `version: ${version}`);
-  text = text.replace(/^serverPath:.*$/m, `serverPath: ${pkgRepo}`);
+    text = text.replace(file.searchRE, file.replacement(version));
 
-  fs.writeFileSync('docs/swanky.config.yaml', text, 'utf8');
+    if (IS_DRYRUN) {
+      console.info(`Updated version in ${file.name}:`);
+      console.info(text);
+      console.info('---------------');
+    } else {
+      fs.writeFileSync(fileName, text, 'utf8');
+      console.log(`Updated version in ${file.name}`);
+    }
+  });
 });
 // END_CONFIT_GENERATED_CONTENT
 
